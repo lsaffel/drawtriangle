a45dae1d0e00b64e5716217816bef051d956192d
Lisa <llvvss@icloud.com>
2024-03-15 15:26:53 -0600

    draw a triangle

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# drawtriangle
+Draw triangle
+Draws a triangle of a variable height  
+with a specific character  
+Like this:  
+ &  
+ & &  
+ & & &  
+ & & & &  
+ & & & & &  
+ & & & & & &  
+ & & & & & & &
--- src/drawtriangle.cpp
+++ src/drawtriangle.cpp
@@ -3,6 +3,20 @@
 using namespace std;
 
 int main() {
+    int length;
+    cout << "Length: " << endl;
+    // cin >> length;
+    length = 7;
+    char symbol;
+    cout << "Symbol: " << endl;
+    // cin >> symbol;
+    symbol = '&';
 
-    
+    for(int i = 1; i <= length; i++) {
+        for(int j = 1; j <= i; j++) {
+            cout << setw(2) << symbol;
+        }
+        cout << endl;
+    }
+    return 0;
 }
